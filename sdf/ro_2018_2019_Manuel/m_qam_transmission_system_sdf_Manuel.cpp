1e6d8f7a5179d8de88c3ae25157cbf3f65ef7da1
msneves <msneves@ua.pt>
2019-03-13 12:04:05 +0000

    Attemp

--- sdf/ro_2018_2019_Manuel/m_qam_transmission_system_sdf_Manuel.cpp
+++ sdf/ro_2018_2019_Manuel/m_qam_transmission_system_sdf_Manuel.cpp
@@ -1,11 +1,10 @@
 # include "../../include/netxpto_20180815.h"
 
 # include "../../include/binary_source_20180815.h"
-# include "../../include/discrete_to_continuous_time_20180815.h"
-# include "../../include/iq_modulator_20180815.h"
 # include "../../include/laser_20180815.h"
-# include "../../include/m_qam_mapper_20180815.h"
-# include "../../include/pulse_shaper_20180815.h"
+# include "../../include/m_qam_transmitter_20180815.h"
+# include "../../include/m_qam_receiver_20180815.h"
+# include "../../include/bit_error_rate_20180815.h"
 # include "../../include/sink_20180815.h"
 
 
@@ -13,8 +12,21 @@
 // ################################### System Input Parameters #########################################
 // #####################################################################################################
 
-double symbolPeriod{ 2e-11 };
+int constellationCardinality{ 4 };	
+double symbolPeriod_s{ 2e-11 };	
 int samplesPerSymbol{ 8 };
+double txLocalOscillatorPower_dBm{ 0 };
+pulse_shapper_filter_type txPulseShapperType{ pulse_shapper_filter_type::RaisedCosine };
+double raisedCosineRollOffFactor{ 0.1 };
+int txPulseShaperLength_symbolPeriods{ 20 };
+double rxLocalOscillatorPower_dBm{ 6 };
+double rxTiAmplifierInputNoisePowerSpectralDensity{ 0 };
+int rxTiAmplifierImpulseResponseTimeLength_symbolPeriods{ 20 };
+double rxTiAmplifierBandwidth{ 50.0e9 };
+double rxThermalNoisePower{ 0 };
+pulse_shapper_filter_type rxElectricalFilterImpulseResponseType{ pulse_shapper_filter_type::RaisedCosine };
+int rxSamplerSamplesToSkip{ (txPulseShaperLength_symbolPeriods - 1)*samplesPerSymbol + (rxTiAmplifierImpulseResponseTimeLength_symbolPeriods *samplesPerSymbol/2)};
+
 
 int main() {
 
@@ -22,63 +34,79 @@ int main() {
 	// ########################### Signals Declaration and Inicialization ##################################
 	// #####################################################################################################
 
-	Binary S0{ "S0.sgn" };
+	Binary BinarySourceOut_0{ "S0_BinarySourceOut_0.sgn" };
 
-	Binary S1{ "S1.sgn" };
-	
-	TimeDiscreteAmplitudeContinuousReal S2{ "S2.sgn" };
-	
-	TimeDiscreteAmplitudeContinuousReal S3{ "S3.sgn" };
-	
-	TimeContinuousAmplitudeContinuousReal S4{ "S4.sgn" };
-	
-	TimeContinuousAmplitudeContinuousReal S5{ "S5.sgn" };
-	
-	TimeContinuousAmplitudeContinuousReal S6{ "S6.sgn" };
-	
-	TimeContinuousAmplitudeContinuousReal S7{ "S7.sgn" };
-	
-	OpticalSignal S8{ "S8.sgn" };
+	Binary BinarySourceOut_1{ "S1_BinarySourceOut_0.sgn" };
 
-	OpticalSignal S9{ "S9.sgn" };
+	OpticalSignal TxLocalOscillatorOut{ "S2_TxLocalOscillatorOut.sgn" };
 	
+	OpticalSignal MQamTransmitterOut{ "S3_MQamTransmitterOut.sgn" };
 	
-	
+	OpticalSignal RxLocalOscillatorOut{ "S4_RxLocalOscillatorOut.sgn" };
+
+	Binary MQamReceiverOut{ "S5_MQamReceiverOut.sgn" };
+
+	Binary BitErrorRateOut{ "S6_BitErrorRateOut.sgn" };
+
+		
 	// #####################################################################################################
 	// ########################### Blocks Declaration and Inicialization ###################################
 	// #####################################################################################################
 
 
-	BinarySource B1{ {}, { &S1, &S0 } };
+	BinarySource BinarySource_{ {}, { &BinarySourceOut_0, &BinarySourceOut_1 } };
+	BinarySource_.setBitPeriod(symbolPeriod_s / log2(constellationCardinality));
 
-	Sink B_1{ { &S0 },{} };
+	Laser TxLocalOscillator_{ {},{ &TxLocalOscillatorOut } };
+	TxLocalOscillator_.setSymbolPeriod(symbolPeriod_s);
+	TxLocalOscillator_.setSamplingPeriod(symbolPeriod_s / samplesPerSymbol);
+	TxLocalOscillator_.setOpticalPower_dBm(txLocalOscillatorPower_dBm);
 
-	MQamMapper B2{ { &S1 }, { &S2, &S3 } };
+	MQamTransmitter MQamTransmitter_{ {&BinarySourceOut_1, &TxLocalOscillatorOut}, {&MQamTransmitterOut} };
+	MQamTransmitter_.setNumberOfSamplesPerSymbol(samplesPerSymbol);
+	MQamTransmitter_.setImpulseResponseTimeLength_symbolPeriods(txPulseShaperLength_symbolPeriods);
+	MQamTransmitter_.setFilterType(txPulseShapperType);
+	MQamTransmitter_.setRollOffFactor(raisedCosineRollOffFactor);
 
-	B2.setIqAmplitudes({ {1,1}, {1,-1}, {-1,1}, {-1,-1} });
+	Laser RxLocalOscillator_{ {},{ &RxLocalOscillatorOut } };
+	RxLocalOscillator_.setSymbolPeriod(symbolPeriod_s);
+	RxLocalOscillator_.setSamplingPeriod(symbolPeriod_s / samplesPerSymbol);
+	RxLocalOscillator_.setOpticalPower_dBm(rxLocalOscillatorPower_dBm);
 
-	DiscreteToContinuousTime B3{ { &S2 }, { &S4 } };
+	MQamReceiver MQamReceiver_{ {&MQamTransmitterOut, &RxLocalOscillatorOut} , {&MQamReceiverOut} };
+	MQamReceiver_.setNoiseSamplingPeriod(symbolPeriod_s / samplesPerSymbol);
+	MQamReceiver_.setNoiseSymbolPeriod(symbolPeriod_s);
+	MQamReceiver_.setPhotodiodesResponsivity(1.0);
+	MQamReceiver_.setGain(1.0);
+	MQamReceiver_.setTiAmplifierFilterType(LowPass);
+	MQamReceiver_.setTiAmplifierCutoffFrequency(rxTiAmplifierBandwidth);
+	MQamReceiver_.setTiAmplifierImpulseResponseTimeLength_symbolPeriods(rxTiAmplifierImpulseResponseTimeLength_symbolPeriods);
+	MQamReceiver_.setAmplifierInputNoisePowerSpectralDensity(rxTiAmplifierInputNoisePowerSpectralDensity);
+	MQamReceiver_.setThermalNoisePower(rxThermalNoisePower);
+	MQamReceiver_.setSamplesToSkip(rxSamplerSamplesToSkip);
+	MQamReceiver_.setFilterType(rxElectricalFilterImpulseResponseType);
+	MQamReceiver_.setRollOffFactor(raisedCosineRollOffFactor);
+	
 
-	DiscreteToContinuousTime B4{ { &S3 }, { &S5 } };
-
-	PulseShaper B5{ { &S4 }, { &S6 } };
-
-	PulseShaper B6{ { &S5 }, { &S7 } };
-
-	Laser B7{ {}, { &S8 } };
-	B7.setSymbolPeriod(symbolPeriod);
-	B7.setSamplingPeriod(symbolPeriod / samplesPerSymbol);
-
-
-	IqModulator B8{ { &S6, &S7, &S8 }, { &S9 } };
-
-	Sink B_8{ { &S9 }, {} };
-
+	BitErrorRate BitErrorRate_{ {&MQamReceiverOut, &BinarySourceOut_0},{ &BitErrorRateOut} };
+	
+	Sink Sink_{ {&BitErrorRateOut},{} };
+	
 	// #####################################################################################################
 	// ########################### System Declaration and Inicialization ###################################
 	// #####################################################################################################
 
-	System MainSystem{ { &B1, &B_1, &B2, &B3, &B4, &B5, &B6, &B7, &B8, &B_8 } };
+	System MainSystem{ 
+		{ 
+			&BinarySource_,
+			&TxLocalOscillator_, 
+			&MQamTransmitter_, 
+			&RxLocalOscillator_, 
+			&MQamReceiver_,
+			&BitErrorRate_,
+			&Sink_
+		}
+	};
 
 	// #####################################################################################################
 	// #################################### System Run #####################################################
